060baae41548f861f0f1237b09dda372bbc5c2f9
Supanut Apikulvanich <supanut.overload@gmail.com>
2021-01-22 11:06:55 +0700

    Update service name caller

--- caller-service/http-call.js
+++ caller-service/http-call.js
@@ -2,18 +2,18 @@ const axios = require('axios');
 
 const calleeUrl = process.env.CALLEE_URL || 'http://localhost:6001/hello';
 
-const NAME = 'Caller';
+const ServiceName = 'Caller';
 const run = async () => {
   const { message } = await axios({
     url: calleeUrl,
     method: 'POST',
     data: {
-      name: NAME,
+      name: ServiceName,
     },
   }).then((response) => response.data);
 
   // test
-  if (message === `Hi ${NAME}`) {
+  if (message === `Hi ${ServiceName}`) {
     console.log('test pass');
   } else {
     console.log('failed');
